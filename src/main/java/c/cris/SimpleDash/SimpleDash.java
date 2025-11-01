08fa61e619b6bd78d2a87ab0a28e3887e4d057a5
W1CK3D <149416505+W1CK3DWORKS@users.noreply.github.com>
2025-11-01 08:12:15 +0100

    Update SimpleDash.java

--- src/main/java/c/cris/SimpleDash/SimpleDash.java
+++ src/main/java/c/cris/SimpleDash/SimpleDash.java
@@ -9,6 +9,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
